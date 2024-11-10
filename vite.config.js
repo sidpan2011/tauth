15c3e94d3efc07e3da19d8c65ab80e3ea6447754
Sidhanth Pandey <sidhanthpandey@192.168.1.4>
2024-11-10 08:33:33 +0530

    Fix UI break issue with vercel deployement

--- vite.config.js
+++ vite.config.js
@@ -2,26 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
-  },
-  build: {
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-        },
-      },
-    },
-  },
-  optimizeDeps: {
-    include: ['axios', 'jotai', 'react-router-dom'],
   },
 })
