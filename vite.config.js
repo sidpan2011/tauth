5e323d5972cd947836df53616984f48fdaf90279
Sidhanth Pandey <sidhanthpandey@192.168.1.4>
2024-11-10 07:36:08 +0530

    Fix deployement issue

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+  optimizeDeps: {
+    include: ['axios', 'jotai', 'react-router-dom'],
+  },
 })
